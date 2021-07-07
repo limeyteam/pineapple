66ed57dbfb11a2c9441fe6c0c21c08752e6bb7eb
MatteeCodes <46092609+MatteeCodes@users.noreply.github.com>
2021-07-07 10:55:36 -0500

    Added some basic VGA text rendering and a hello world along with some testing

---
+++ build.sh
@@ -0,0 +1,2 @@
+cargo bootimage
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-spinix/debug/bootimage-spinix.bin -L "C:\Program Files\qemu"
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
