686188218d2a86f67c5007c070ac408290161f99
MatteeCodes <46092609+MatteeCodes@users.noreply.github.com>
2021-07-11 18:59:02 -0500

    Added support for some basic UEFI things and hopefully more advanced things sooner

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,24 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(spinix::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-
-use bootloader::BootInfo;
-use bootloader::entry_point;
-use spinix::println;
 use core::panic::PanicInfo;
+use bootloader::{entry_point, BootInfo};
 
 entry_point!(kernel_main);
 
-fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use spinix::memory::active_level_4_table;
-    use x86_64::VirtAddr;
 
-    use spinix::memory::translate_addr;
-
-    spinix::init();
-
- let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    /*
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
-
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
-        println!("{:?} -> {:?}", virt, phys);
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        let mut value = 0x0;
+        for byte in framebuffer.buffer_mut() {
+            *byte = value;
+            value = value.wrapping_add(1);
+        }
     }
-    */
-
-    spinix::collectionTask();
-
-    // Test here
-    #[cfg(test)]
-    test_main();
-
-    spinix::hlt_loop();
+    loop {}
 }
-
 
 /// This function is called on panic.
-#[cfg(not(test))]
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    spinix::hlt_loop();
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    spinix::test_panic_handler(info);
-    spinix::hlt_loop();
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
